033b25f36206b79c321e25e4d9f25185f9130a90
banana <banana@eurus.cn>
2018-04-11 16:34:12 +0800

    add func2

---
+++ func2/func.js
@@ -0,0 +1,12 @@
+var fdk = require('@fnproject/fdk');
+
+fdk.handle(function(input) {
+    var name = 'World';
+    if (input.name) {
+        name = input.name;
+    }
+    response = {
+        'message': `This is func2, ${name}.`
+    }
+    return response
+})
