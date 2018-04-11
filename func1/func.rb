29c6c7d4859b5b1d3cea1fff433be01e83ba9f8c
banana <banana@eurus.cn>
2018-04-11 16:32:22 +0800

    add func1

---
+++ func1/func.rb
@@ -0,0 +1,13 @@
+require 'fdk'
+
+def myhandler(context, input)
+	STDERR.puts "call_id: " + context.call_id
+	name = "World"
+	nin = input['name']
+	if nin && nin != ""
+		name = nin
+	end
+	return {message: "This is func1, #{name}."}
+end
+
+FDK.handle(:myhandler)
