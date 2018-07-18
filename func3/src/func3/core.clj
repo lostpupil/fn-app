a9c313e48279277e8b2193965d50b06d6fbd877a
banana <banana@eurus.cn>
2018-07-18 16:00:52 +0800

    update example

--- func3/src/func3/core.clj
+++ func3/src/func3/core.clj
@@ -1,6 +1,17 @@
 (ns func3.core
   (:gen-class))
 
+; name = "World";
+; fs = require('fs');
+; try {
+;   input = fs.readFileSync('/dev/stdin').toString();
+;   if (input) {
+;     name = input;
+;   }
+; } catch(e) {}
+; console.log("Hello", name, "from Node!");
+
+;; Read user input from stdin
 (defn -main []
   "I don't do a whole lot."
   (println "Hello, World!"))
